49df6bd2c6e9edff7231c632a8c900bedaecf103
chosungsu <ski06043@naver.com>
2022-05-11 13:17:08 +0900

    commit mistakes

--- lib/Dialogs/destroyBackKey.dart
+++ lib/Dialogs/destroyBackKey.dart
@@ -3,26 +3,29 @@ import 'package:flutter/services.dart';
 import 'package:hive_flutter/adapters.dart';
 
 destroyBackKey(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: const Text('종료'),
-      content: const Text('앱을 종료하시겠습니까?'),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () => Navigator.pop(context, false),
-          child: const Text('아니요'),
-        ),
-        TextButton(
-          onPressed: () {
-            Hive.box('user_info').get('autologin') == false
-                ? Hive.box('user_info').delete('id')
-                : null;
-            SystemNavigator.pop();
-          },
-          child: const Text('네'),
-        ),
-      ],
-    ),
-  );
+  Hive.box('user_setting').get('login_delete') == 1 ||
+          Hive.box('user_setting').get('login_delete') == null
+      ? showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('종료'),
+            content: const Text('앱을 종료하시겠습니까?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, false),
+                child: const Text('아니요'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Hive.box('user_info').get('autologin') == false
+                      ? Hive.box('user_info').delete('id')
+                      : null;
+                  SystemNavigator.pop();
+                },
+                child: const Text('네'),
+              ),
+            ],
+          ),
+        )
+      : Navigator.pop(context);
 }
