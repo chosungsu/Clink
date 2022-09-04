303e13891dae74e2ae38a8b8d44d177659229455
chosungsu <ski06043@naver.com>
2022-09-04 19:36:26 +0900

    change ui codes

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ import 'Tool/BGColor.dart';
 
 const Map<String, String> UNIT_ID = kReleaseMode
     ? {
-        'ios': 'ca-app-pub-5775667984133884~4968515094',
+        'ios': 'ca-app-pub-5775667984133884~4010656648',
         'android': 'ca-app-pub-5775667984133884~4968515094',
       }
     : {
