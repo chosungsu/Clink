06b2f3809313a9d9dafa7c9d2fb21a7b91be6a11
chosungsu <ski06043@naver.com>
2022-09-21 21:20:36 +0900

    change ui codes

--- lib/Auth/GoogleSignInController.dart
+++ lib/Auth/GoogleSignInController.dart
@@ -1,12 +1,15 @@
 import 'package:clickbyme/Tool/FlushbarStyle.dart';
+import 'package:clickbyme/UI/Sign/UserCheck.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hive/hive.dart';
+import 'package:page_transition/page_transition.dart';
 
 import '../Tool/Getx/PeopleAdd.dart';
+import '../route.dart';
 
 class GoogleSignInController extends GetxController {
   final _googleSignIn = GoogleSignIn();
@@ -21,10 +24,10 @@ class GoogleSignInController extends GetxController {
     String email = googleSignInAccount!.email.toString();
 
     //내부 저장으로 로그인 정보 저장
-    Hive.box('user_info').put('id', nick);
-    Hive.box('user_info').put('email', email);
-    Hive.box('user_info').put('count', count);
-    Hive.box('user_info').put('autologin', ischecked);
+    await Hive.box('user_info').put('id', nick);
+    await Hive.box('user_info').put('email', email);
+    await Hive.box('user_info').put('count', count);
+    await Hive.box('user_info').put('autologin', ischecked);
     String codes = Hive.box('user_info').get('id').toString().length > 5
         ? Hive.box('user_info').get('email').toString().substring(0, 3) +
             Hive.box('user_info')
@@ -41,22 +44,24 @@ class GoogleSignInController extends GetxController {
                 .substring(0, 2) +
             Hive.box('user_info').get('id').toString().substring(0, 2);
     //firestore 저장
-    firestore.collection('User').doc(nick).get().then((value) {
+    firestore.collection('User').doc(nick).get().then((value) async {
       Snack.show(
           title: '로딩중',
           snackType: SnackType.waiting,
           content: '로그인중입니다.잠시만 기다려주세요',
           context: context);
       if (value.exists) {
-        firestore.collection('User').doc(nick).update({
+        await firestore.collection('User').doc(nick).update({
           'name': nick,
           'email': email,
           'login_where': 'google_user',
           'autologin': ischecked,
           'code': codes
+        }).whenComplete(() {
+          GoToMain(context);
         });
       } else {
-        firestore.collection('User').doc(nick).set({
+        await firestore.collection('User').doc(nick).set({
           'name': nick,
           'subname': nick,
           'email': email,
@@ -64,7 +69,9 @@ class GoogleSignInController extends GetxController {
           'time': DateTime.now(),
           'autologin': ischecked,
           'code': codes
-        }, SetOptions(merge: true));
+        }, SetOptions(merge: true)).whenComplete(() {
+          GoToMain(context);
+        });
       }
     });
 
--- lib/Page/LoginSignPage.dart
+++ lib/Page/LoginSignPage.dart
@@ -181,9 +181,7 @@ class _LoginSignPageState extends State<LoginSignPage>
               /*await Provider.of<GoogleSignInController>(context, listen: false)
                   .login(context, ischecked);*/
               GoogleSignInController().login(context, ischecked);
-              String name = Hive.box('user_info').get('id');
-              String subname = '';
-              await Navigator.of(context).pushReplacement(
+              /*await Navigator.of(context).pushReplacement(
                 PageTransition(
                   type: PageTransitionType.bottomToTop,
                   child: const MyHomePage(
@@ -191,7 +189,7 @@ class _LoginSignPageState extends State<LoginSignPage>
                   ),
                 ),
               );
-              Hive.box('user_setting').put('page_index', 1);
+              Hive.box('user_setting').put('page_index', 1);*/
             },
             child: SizedBox(
               width: 200 * (MediaQuery.of(context).size.width / 392),
