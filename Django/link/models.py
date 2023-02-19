2ae2f3cbef40d941680736421c2b346d2c6ce16a
chosungsu <ski06043@naver.com>
2023-02-19 21:00:55 +0900

    move backend part another github

--- Django/link/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    #nick, email, code
-    nick = models.CharField(max_length=100)
-    email = models.TextField()
-    code = models.TextField()
--- Django/link/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path, include
-from .views import UserAPI
-urlpatterns = [
-    path('user/', UserAPI)
-]
